758ecad52c137bf2fbb8865fae9538d806fbf783
Michael Lima <michael.cdl@outlook.com>
2021-04-01 15:22:25 -0500

    update classe: Treinador :hammer:

--- BatalhaDeMonstro/src/br/com/anhembi/batalha/model/Treinador.java
+++ BatalhaDeMonstro/src/br/com/anhembi/batalha/model/Treinador.java
@@ -1,9 +1,32 @@
 package br.com.anhembi.batalha.model;
 /**
- * 
  * @author Michael Carlos
- *
  */
 public class Treinador {
+  private Monstros alpha;
+  private Monstros beta;
 
+  public Treinador(String alpha, String beta) {
+      this.alpha = new Monstros(alpha);
+      this.beta = new Monstros(beta);
+  }
+
+  public int getMonsterVida(){
+    return getMonstroAtivo().getVida();
+  }
+
+  public ???? getMonsterAttack(){
+    return getMonstroAtivo().getAttack();
+  }
+
+  public String getMonstroTipo() {
+    return getMonstroAtivo().getTipo();
+  }
+
+  public Monstros getMonstroAtivo() {
+    if(alpha.getVida() < 0){
+      return alpha;
+    }
+    return beta;
+  }
 }
