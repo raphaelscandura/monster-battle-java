2d4f9532c3330eb336d41fa9a4921544725447e6
SabFernandes <70286532+SabFernandes@users.noreply.github.com>
2021-04-04 14:46:14 -0300

    Construindo Metodos e funções

--- BatalhaDeMonstro/src/br/com/anhembi/batalha/model/Monstros.java
+++ BatalhaDeMonstro/src/br/com/anhembi/batalha/model/Monstros.java
@@ -5,6 +5,7 @@ package br.com.anhembi.batalha.model;
  *
  */
 public class Monstros {
+    String nomeMonstro;
     String tipo;
     String ataques1;
     String ataques2;
@@ -14,10 +15,17 @@ public class Monstros {
     int velocidade;
     String fraqueza;
 
-    public Monstros(){
+    public Monstros(String nomeMontro, String tipo, String ataques1,String ataques2, int vida, int força, 
+    int defesa, int velocidade){
+        this.nomeMonstro;
+        this.tipo;
+        this.ataques1;
+        this.ataques2;
+        this.vida;
+        this.força;
+        this.defesa;
+        this.velocidade;
         
-
-
     }
 
     public int getVida(){
