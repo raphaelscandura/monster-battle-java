e263e4fc282dc9e7dec95371cb900cfaa33f72f7
SabFernandes <70286532+SabFernandes@users.noreply.github.com>
2021-04-07 10:03:21 -0300

    Metodos e Funções :hammer:

--- BatalhaDeMonstro/src/br/com/anhembi/batalha/model/Monstros.java
+++ BatalhaDeMonstro/src/br/com/anhembi/batalha/model/Monstros.java
@@ -5,26 +5,80 @@ package br.com.anhembi.batalha.model;
  *
  */
 public class Monstros {
-    String nomeMonstro;
-    String tipo;
-    String ataques1;
-    String ataques2;
-    int vida;
-    int força;
-    int defesa;
-    int velocidade;
+    private String nomeMonstro;
+    private String status;
+    private String ataques1;
+    private String ataques2;
+    private int temvida;
+    private int força;
+    private int defesa;
+    private int velocidade;
     String fraqueza;
-
-    public void classificaMonstros(String nomeMontro, String tipo, String ataques1, String ataques2, int vida,
-    int força, int defesa, int velocidade){
-        this.nomeMonstro;
-        this.tipo;
-        this.ataques1;
-        this.ataques2;
-        this.vida;
-        this.força;
-        this.defesa;
-        this.velocidade;
+    
+        public String getNomeMonstro() {
+            return nomeMonstro;
+        }
+    
+        public void setNomeMonstro(String nomeMonstro) {
+            this.nomeMonstro = nomeMonstro;
+        }
+    
+        public String getstatus() {
+            return status;
+        }
+    
+        public void setstatus(String status) {
+            this.status = status;
+        }
+    
+        public String getAtaques1() {
+            return ataques1;
+        }
+    
+        public void setAtaques1(String ataques1) {
+            this.ataques1 = ataques1;
+        }
+    
+        public String getAtaques2() {
+            return ataques2;
+        }
+    
+        public void setAtaques2(String ataques2) {
+            this.ataques2 = ataques2;
+        }
+    
+        public int gettemVida() {
+            return temvida;
+        }
+    
+        public void settemVida(int vida) {
+            this.temvida = vida;
+        }
+    
+        public int getForça() {
+            return força;
+        }
+    
+        public void setForça(int força) {
+            this.força = força;
+        }
+    
+        public int getDefesa() {
+            return defesa;
+        }
+    
+        public void setDefesa(int defesa) {
+            this.defesa = defesa;
+        }
+    
+        public int getVelocidade() {
+            return velocidade;
+        }
+    
+        public void setVelocidade(int velocidade) {
+            this.velocidade = velocidade;
+        }
+        
     }
 
     public int getVida(){
