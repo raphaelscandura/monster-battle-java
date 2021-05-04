ad05140b2a191fda168746abf13b2be04bfa793b
Michael Lima <michael.cdl@outlook.com>
2021-05-04 14:53:34 -0500

    Menu Batalha

--- BatalhaDeMonstro/src/br/com/anhembi/batalha/view/MenuBatalha.java
+++ BatalhaDeMonstro/src/br/com/anhembi/batalha/view/MenuBatalha.java
@@ -1,51 +1,20 @@
 package br.com.anhembi.batalha.view;
 import br.com.anhembi.batalha.model.*;
-
+import br.com.anhembi.batalha.control.Ataque;
+    
 public class MenuBatalha {
-   
     public static void main(String[] args) {
         Treinador Bob = new Treinador();
         Treinador Gob = new Treinador();
         Interface inter = new Interface(Bob,Gob);
 
-        Monstros alpha = new Liquides();
-        Monstros beta = new Rockin();
-        Monstros omega = new Flair();
-        
-        Bob.addMonstro(alpha);
-        Bob.addMonstro(alpha) ; 
-        Bob.addMonstro(alpha);
-
-        Gob.addMonstro(omega);
-        Gob.addMonstro(omega) ;
-        Gob.addMonstro(omega);
-
-        inter.imprimirTela();
-
-        // String nome = Bob.getMonstro(0).getNomeMonstro();
-        // int defesa = Bob.getMonstro(0).getDefesa();
-        // int ataque = Bob.getMonstro(0).getForça();
-        // System.out.println(nome);
-        // System.out.println("Defesa: " + defesa);
-        // System.out.println("Ataque: " + ataque + "\n\n");
-        
-        // nome = Bob.getMonstro(1).getNomeMonstro();
-        // defesa = Bob.getMonstro(1).getDefesa();
-        // ataque = Bob.getMonstro(1).getForça();
-        // System.out.println(nome);
-        // System.out.println("Defesa: " + defesa);
-        // System.out.println("Ataque: " + ataque + "\n\n");
-        
-        // nome = Bob.getMonstro(2).getNomeMonstro();
-        // defesa = Bob.getMonstro(2).getDefesa();
-        // ataque = Bob.getMonstro(2).getForça();
-        // System.out.println(nome);
-        // System.out.println("Defesa: " + defesa);
-        // System.out.println("Ataque: " + ataque + "\n\n");
-        
-        // nome = Bob.getMonstroAtivo().getNomeMonstro();
-        // System.out.println(nome);
+        for(int i = 0; i < 3; i++){
+            Bob.addMonstro(inter.printEscolhas(i));
+            System.out.println("i:  " + i);
+            System.out.println("Nome Monstro:      " + Bob.getMonstro(i).getNomeMonstro());
+            System.out.println("Ataque 1:          " + Bob.getMonstro(i).getAtaques1().getNome() +"[" + Bob.getMonstro(i).getAtaques1().getPoder() + "] --" + Bob.getMonstro(i).getAtaques1().getTipo());
+            System.out.println("Ataque 1:          " + Bob.getMonstro(i).getAtaques2().getNome() +"[" + Bob.getMonstro(i).getAtaques2().getPoder() + "] --" + Bob.getMonstro(i).getAtaques2().getTipo());
+        }
     }
-
 }
 
